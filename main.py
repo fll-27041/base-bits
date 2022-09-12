f53f912ef720ca85459723d873e4c84d3b4b500d
fll-27041-coach <fll-27041-coach@example.com>
2022-09-11 22:48:53 -0400

    Change line endings for code from CRLF to LF.

--- main.py
+++ main.py
@@ -26,19 +26,95 @@ class BaseBits:
     initialization, and top-level error handling""" 
     def __init__(self):
         """Initializes new instance of the class and sets up hardware"""
-        self.ev3 = EV3Brick()
-        self.motorA = Motor(Port.A)
-        self.motorB = Motor(Port.B)
-        self.motorC = Motor(Port.C)
-        self.motorD = Motor(Port.D)
+        try:
+            self.ev3 = EV3Brick()  
+            self.ev3.light.on(Color.GREEN)
+            self.ev3.speaker.beep(frequency=400, duration=200)
+        except:
+            self.ev3.light.on(Color.RED)
+            self.ev3.screen.draw_text(0,40,"BRICK ERROR")
+            self.ev3.print("BRICK ERROR")
+            self.ev3.speaker.beep(frequency=2000, duration=2000)
+            wait(2000)
+            sys.exit()
 
-        self.robot = DriveBase(self.motorB, self.motorC, wheel_diameter=56,
-                                axle_track=152)
-        self.robot.settings(straight_speed=200, straight_acceleration=100,
-                            turn_rate=100, turn_acceleration=100)
+        try:
+            self.left_attachment_motor = Motor(Port.A)
+        except:
+            self.ev3.light.on(Color.RED)
+            self.ev3.screen.draw_text(0,40,"PORT A MOTOR")
+            print("PORT A MOTOR ERROR")
+            self.ev3.speaker.beep(frequency=2000, duration=2000)
+            wait(2000)
+            sys.exit()
+    
+        try:
+            self.left_drive_motor = Motor(Port.B,positive_direction=Direction.COUNTERCLOCKWISE)
+        except:
+            self.ev3.light.on(Color.RED)
+            self.ev3.screen.draw_text(0,40,"PORT B MOTOR")
+            print("PORT B MOTOR ERROR")
+            self.ev3.speaker.beep(frequency=2000, duration=2000)
+            wait(2000)
+            sys.exit()
 
-        self.sensor1 = ColorSensor(Port.S1)
-        self.sensor4 = ColorSensor(Port.S4)
+        try:
+            self.right_drive_motor = Motor(Port.C,positive_direction=Direction.COUNTERCLOCKWISE)
+        except:
+            self.ev3.light.on(Color.RED)
+            self.ev3.screen.draw_text(0,40,"PORT C MOTOR")
+            print("PORT C MOTOR ERROR")
+            self.ev3.speaker.beep(frequency=2000, duration=2000)
+            wait(2000)
+            sys.exit()
+
+        try:    
+            self.right_attachment_motor = Motor(Port.D)
+        except:
+            self.ev3.light.on(Color.RED)
+            self.ev3.screen.draw_text(0,40,"PORT D MOTOR")
+            print("PORT D MOTOR ERROR")
+            self.ev3.speaker.beep(frequency=2000, duration=2000)
+            wait(2000)
+            sys.exit()
+
+        try:
+            self.robot = DriveBase(self.left_drive_motor, self.right_drive_motor, wheel_diameter=88, axle_track=111)
+            self.robot.settings(straight_speed=600, straight_acceleration=200, turn_rate=200, turn_acceleration=100)
+        except:
+            self.ev3.light.on(Color.RED)
+            self.ev3.screen.draw_text(0,40,"DRIVEBASE ERROR")
+            print("DRIVEBASE ERROR")
+            self.ev3.speaker.beep(frequency=2000, duration=2000)
+            wait(2000)
+            sys.exit()
+
+        try: 
+            self.left_color_sensor = ColorSensor(Port.S1)
+        except:
+            self.ev3.light.on(Color.RED)
+            self.ev3.screen.draw_text(0,40,"PORT 1 SENSOR")
+            print("PORT 1 SENSOR ERROR")
+            self.ev3.speaker.beep(frequency=2000, duration=2000)
+            wait(2000)
+            sys.exit()
+
+        try:
+            self.right_color_sensor = ColorSensor(Port.S4)
+        except:
+            self.ev3.light.on(Color.RED)
+            self.ev3.screen.draw_text(0,40,"PORT 4 SENSOR")
+            print("PORT 4 SENSOR ERROR")
+            self.ev3.speaker.beep(frequency=2000, duration=2000)
+            wait(2000)
+            sys.exit()
+
+        self.ev3.screen.clear()
+        self.ev3.light.off()
+        self.ev3.light.on(Color.GREEN)
+        self.ev3.screen.draw_text(10,40,"STARTUP GOOD!")
+        wait(1000)
+        self.ev3.screen.clear()   
 
         # Represents what page of the menu we are on
         self.page = 0
