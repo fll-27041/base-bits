5ac2ad01d2169b983a13bdc8f135db2b30642619
Mike <mike.n48mh@gmail.com>
2022-09-28 13:12:40 -0400

    Commit with values shown on display

--- missions/mission7.py
+++ missions/mission7.py
@@ -7,9 +7,10 @@ def run(bb: BaseBits):
     bb.ev3.speaker.beep()  # Intro beep
 
     print("Place both on black")
+    bb.ev3.screen.draw_text(0,20,"Sensors on Black")
     
     for i in range(3):  
-        bb.ev3.speaker.beep(400 - 50 * i)
+        bb.ev3.speaker.beep(800 - 100 * i)
         wait(1000)
 
     left_sum_reflectivity = 0
@@ -25,11 +26,14 @@ def run(bb: BaseBits):
 
     print("minimums", left_min_reflectivity, right_min_reflectivity)
 
+    bb.ev3.screen.clear()
+    bb.ev3.screen.draw_text(0,20,"Sensors on White")
+
     bb.ev3.speaker.beep()  # Done with minimums, move robot
     bb.robot.straight(-50)  # Back robot off of black line
     
     for i in range(3):  
-        bb.ev3.speaker.beep(400 - 50 * i)
+        bb.ev3.speaker.beep(800 - 100 * i)
         wait(1000)
 
     for n in range(100):  # Find maximum reflectivty
@@ -42,60 +46,32 @@ def run(bb: BaseBits):
 
     print("maximums", left_max_reflectivity, right_max_reflectivity)
 
-    bb.ev3.speaker.beep(1000, 3000)  # Done
-
     left_average_reflectivity = (left_max_reflectivity + left_min_reflectivity)/2  # Calculate average value between white and black for line following
     right_average_reflectivity = (right_max_reflectivity + right_min_reflectivity)/2  # Calculate average value between white and black for line following
     
     print("Average values ", left_average_reflectivity, right_average_reflectivity)
 
+    bb.ev3.screen.clear()  # Clear screen before drawing results
 
-    # bb.robot.drive(30,0)  # Start robot driving straight at medium speed ---------------------------------------
+    bb.ev3.screen.draw_text(0,0,"Average Values")
 
-    # left_max_refl = 50   # Variable for highest left reflectivity
-    # right_max_refl = 50   # Variable for highest right reflectivity
+    bb.ev3.screen.draw_text(0,30,"PRT1")  # Draw top line of headers
+    bb.ev3.screen.draw_text(100,30, int(left_average_reflectivity))
+    bb.ev3.screen.draw_text(0,60,"PRT4")
+    bb.ev3.screen.draw_text(100,60, int(right_average_reflectivity))
+    bb.ev3.screen.draw_text(0,90,"Cntr Bttn to Stop")
 
-    # while True:  # Drive until sensor at near edge of black and save highest reflectivity
+    pressed = []  # Check if a button has been pressed
 
-    #     left_reflectivity = bb.left_color_sensor.reflection()
-    #     if left_reflectivity > left_max_refl:     # Test if new highest reflectivity
-    #         left_max_refl = left_reflectivity     # Save highest value
-        
-    #     right_reflectivity = bb.right_color_sensor.reflection()
-    #     if right_reflectivity > right_max_refl:     # Test if new highest reflectivity
-    #         right_max_refl = right_reflectivity     # Save highest value
+    while len(pressed) != 1:  # Loop until a button is pressed
+        pressed = bb.ev3.buttons.pressed()
 
-    #     if left_reflectivity < 50:    # End loop when at near edge of black
-    #         break
+    button = pressed[0]  # Remember which button was pressed
+    bb.ev3.screen.clear()  # Clear the screen so it is cleabn for selected program
+    bb.ev3.speaker.beep(1500, 100)  # Let user know a button was pressed
 
-    # print("At edge of black, highest value ", left_max_refl, right_max_refl)
-
-    # bb.ev3.speaker.beep()    # At edge of black 
-
-    # left_min_refl = 50   # Variable for lowest left reflectivity
-    # right_min_refl = 50   # Variable for lowest right reflectivity          
-
-    # while True:  # Drive until sensor at far edge of black and save lowest reflectivity
-
-    #         left_reflectivity = bb.left_color_sensor.reflection()
-    #         if left_reflectivity < left_min_refl:     # Test if new lowest reflectivity
-    #             left_min_refl = left_reflectivity     # Save lowest value
-            
-    #         right_reflectivity = bb.right_color_sensor.reflection()
-    #         if right_reflectivity < right_min_refl:     # Test if new lowest reflectivity
-    #             right_min_refl = right_reflectivity     # Save lowest value
-
-    #         if left_reflectivity > 50:    # End loop when at near edge of black
-    #             break
-
-    # print("At edge of black, lowest value ", left_min_refl, right_min_refl)
-
-    # left_average_refl = (left_max_refl + left_min_refl)/2  # Calculate average value between white and black for line following
-    # right_average_refl = (right_max_refl + right_min_refl)/2  # Calculate average value between white and black for line following
+    # Now wait for the button to be released before starting selected code
+    while any(ev3.buttons.pressed()):  # Don't start new program until button released
+        pass
     
-    # print("Average values ", left_average_refl, right_average_refl)
-
-    # bb.robot.straight(20) # Drive futher so when robot pivots the sensor will be on the edge and pointed along the line
-    # bb.ev3.speaker.beep()
-
-
+    bb.ev3.speaker.beep()  # Done, return to menu
