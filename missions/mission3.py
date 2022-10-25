9d9c340e1d6c6e70d50cfc973f36c2adc4a3e6b1
Mike <mike.n48mh@gmail.com>
2022-10-25 10:35:51 -0400

    Commit M3 changes

--- missions/mission3.py
+++ missions/mission3.py
@@ -1,2 +1,10 @@
-def run():
+from pybricks.tools import wait, StopWatch, DataLog
+
+def run(bb: BaseBits):
     print("Running mission 3")
+    wait(1000)              # Tuesday first change M3
+    bb.ev3.speaker.beep()   # Tuesday first change M3
+    wait(1000)              # Tuesday first change M3
+    bb.ev3.speaker.beep()   # Tuesday first change M3
+    wait(1000)              # Tuesday first change M3
+    bb.ev3.speaker.beep()   # Tuesday first change M3
