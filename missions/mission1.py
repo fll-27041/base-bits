4e7c121568ea6fab42b8bafd5a64f2ccd5cf10be
Michael <n48mh@comcast.net>
2022-10-25 09:53:55 -0400

    Commit M1 change made on laptop

--- missions/mission1.py
+++ missions/mission1.py
@@ -1,8 +1,3 @@
 def run(bb: BaseBits):
     print("Running mission 1")
-    for i in range(2):
-        bb.ev3.speaker.beep(frequency=2000, duration=500)
-    for i in range(2):
-        bb.ev3.speaker.beep(frequency=5000, duration=500)
-# Temp change
-# And another
+    bb.ev3.beep()   # Tuesday first change M1
